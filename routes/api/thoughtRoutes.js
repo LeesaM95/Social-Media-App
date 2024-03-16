d592b5e4cf5cc4b43b9ec189bf13151d45ffe329
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2024-03-15 23:02:41 -0400

    updated routes js, utils js, package.json, and index.js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtControllers');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/thought:id
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtID/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughts/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
---
+++ utils/data.js
@@ -0,0 +1,36 @@
+const usernames = [
+    {
+        "username": "BookatiPidove",
+        "email": "RanchonoDorito@gmail.com",
+        "thoughts": [],
+        "friends": [],
+        "__V": 0
+    },
+    {
+        "username": "ItsYoshiii",
+        "email": "MAlvarez1993@outlook.com",
+        "thoughts": [],
+        "friends": [],
+        "__V": 0
+    }
+];
+
+const possibleThoughts = [
+{
+    "thoughtText": "Jail and prison mean the same thing, but when you add 'er' to the end of them they become opposites.",
+    "username": "BookatiPidove",
+    "createdAt": "2024-03-15T10:57.23+05:00",
+    "reactions": [
+        {
+            "reactionBody": "",
+            "username": "",
+            "reactionId": [],
+            "createdAt": ""
+        }
+    ],
+    "__v":0
+
+},
+
+];
+
